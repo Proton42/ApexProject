f4a89cac2499ded853bffdf969d4713bfbe754aa
jokullj <jokullj@gmail.com>
2016-03-14 11:21:07 +0000

    Update DayTourGogn.java

--- src/apexproject/data/DayTourGogn.java
+++ src/apexproject/data/DayTourGogn.java
@@ -1,10 +1,13 @@
-package Gogn;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+package apexproject.data;
+
+import apexproject.DayTours;
 
 /**
  *
